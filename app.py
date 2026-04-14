e5a582508f4798a5f90e7563030fe14478bf1bbc
hernanelena1971-gif <hernanelena1971@gmail.com>
2026-04-14 10:23:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -246,14 +246,14 @@ def generar_pdf_informe(row, codigo_sitio):
     # LOGOS (estable, sin bug)
     # --------------------------------------------------
     def img(path, target_width=110, min_height=45):
-    if not os.path.exists(path):
-        return Spacer(target_width, min_height)
-
-    img = Image(path)
-    ratio = target_width / img.imageWidth
-    img.drawWidth = target_width
-    img.drawHeight = img.imageHeight * ratio
-    return img
+        if not os.path.exists(path):
+            return Spacer(target_width, min_height)
+    
+        img = Image(path)
+        ratio = target_width / img.imageWidth
+        img.drawWidth = target_width
+        img.drawHeight = img.imageHeight * ratio
+        return img
 
     
 header = Table(
